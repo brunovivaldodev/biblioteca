0a7c70e2a6319896a2a09b1840c8ada3fb5323cb
Bruno Vivaldo <brunolumeca@live.com>
2022-07-12 17:01:23 +0100

    Borrow Model Refact Done

--- src/daos/Borrow.js
+++ src/daos/Borrow.js
@@ -1,11 +1,16 @@
+import Borrow from "../models/Borrow.js";
 
 let borrows = []
 export default class BorrowDAO {
+    static count = 0;
     static create(borrow) {
 
-        borrows.push(borrow)
+        const createdBorrow = new Borrow(this.count + 1, borrow.user, borrow.borrow, borrow.amount)
 
-        return borrows
+        borrows.push(createdBorrow)
+        this.count++
+
+        return createdBorrow
     }
     static index() {
         return borrows
--- src/models/Borrow.js
+++ src/models/Borrow.js
@@ -1,7 +1,9 @@
 
-class Borrow {
-    constructor(user,material){
+export default class Borrow {
+    constructor(identifier, user, material, amount) {
+        this.identifier = identifier
         this.user = user
         this.material = material
+        this.amount = amount
     }
 }
